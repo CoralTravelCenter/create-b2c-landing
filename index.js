a6f71801816549abddfb03fe5b5de19dfe6b4caf
shooore <shooore@gmail.com>
2023-02-19 20:50:27 +0300

    dev

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log("+++ create-b2c-landing: %o", process.cwd());
+const { path: app_root_path } = require("app-root-path");
+const path = require('path')
+const copy = require('recursive-copy');
+
+const cwd = process.cwd();
+
+console.log("+++ create-b2c-landing: %o", cwd);
+
+copy(path.join(app_root_path, 'project-scaffold', '**'), cwd, (error, results) => {
+    if (error) {
+        console.error('Copy failed: ' + error);
+    } else {
+        console.info('Copied ' + results.length + ' files');
+    }
+});
