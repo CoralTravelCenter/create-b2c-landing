b3fd93b4a47ae6ef380b94f888f26595ae9f4481
shooore <shooore@gmail.com>
2023-02-20 22:29:36 +0300

    dev

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cwd = process.cwd();
 
 console.log("+++ create-b2c-landing: %o", cwd);
 
-copy(path.join(__dirname, 'project-scaffold'), cwd, (error, results) => {
+copy(path.join(__dirname, 'project-scaffold'), cwd, { dot: true }, (error, results) => {
     if (error) {
         console.error('Copy failed: ' + error);
     } else {
