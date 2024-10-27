2d99cbec7b183c7727d2a36512f1910add09531a
Mike <mikhail-gankin@yandex.ru>
2024-10-27 15:09:39 +0300

    +++ Upd
    1) Исправлены ошибки в CSS
    2) Отредактирован файл utils.js
    3) Добавлен экспериментальный скрипт, для сжатия и конвертации изображений

--- project-scaffold/site/coral.ru/js/main.js
+++ project-scaffold/site/coral.ru/js/main.js
@@ -1,12 +1,8 @@
 import {
 	hostReactAppReady,
 	vimeoAutoPlay,
-	setH1UnderKV,
 } from "../../common/js/utils";
 
 hostReactAppReady().then(() => {
 	vimeoAutoPlay();
-
-	// Функция прячет системный заголовок под баннер
-	setH1UnderKV();
 });
--- project-scaffold/site/sunmar.ru/js/main.js
+++ project-scaffold/site/sunmar.ru/js/main.js
@@ -1,10 +1,8 @@
 import {
 	hostReactAppReady,
 	vimeoAutoPlay,
-	setH1UnderKV,
 } from "../../common/js/utils";
 
 hostReactAppReady().then(() => {
 	vimeoAutoPlay();
-	setH1UnderKV();
 });
